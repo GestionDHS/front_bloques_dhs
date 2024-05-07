2bf75d10fdb97f73d8c76bc23dc76472e07b726e
María Pía Achigar <achigarpia@gmail.com>
2024-05-07 15:17:59 -0300

    structure fixes

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
 
 document.querySelector('#app').innerHTML = `
   <main>
@@ -14,8 +12,8 @@ document.querySelector('#app').innerHTML = `
       <li id="alternar">Alternar</li>
     </ul>
    </nav>
-   <secrion class="contenedor">
-      <div class="bloque_codigo felx_col">
+   <secrion class="contenedor" id="contenedor">
+      <div class="bloque_codigo felx_col content-box" id="bloque_codigo">
          <div class="head_bloque_codigo">
             <ul class="ul_nav_secundario">
                <li><button id="btn-html" class="button button_focus btn_relative">HTML</button></li>
@@ -24,15 +22,15 @@ document.querySelector('#app').innerHTML = `
             </ul>
          </div>
          <div class="body_bloque_codigo">
-            <div class="body_bloque flex_col">
+            <div class="body_bloque flex_col" id="body_bloque">
                <div class="ventana_bloques flex_row">
                   <div id="wsp-html">Workspace HTML</div>
                   <div id="wsp-css" class="hidden">Workspace CSS</div>
                   <div id="wsp-js" class="hidden">Workspace JS</div>
                </div>
-               <div class="resizer_bloques resizer_css"></div>
+               <div class="resizer_css hidden" id="r2"></div>
             </div>
-            <div class="body_codigo flex_col hidden">
+            <div class="body_codigo flex_col hidden" id="body_codigo">
                <div class="ventana_codigo flex_row">
                   <div id="codigo-html">codigo HTML</div>
                   <div id="codigo-css" class="hidden">codigo CSS</div>
@@ -40,13 +38,13 @@ document.querySelector('#app').innerHTML = `
                </div>
             </div>
          </div>
-         <div class="resizer_bloques_codigo resizer_css"></div>
+         <div class="resizer_css resizer" id="r1"></div>
       </div>
       
-      <div class="navegador flex_row">
+      <div class="navegador flex_row " id="navegador">
          <div class="ventana_navegador">
             <h2>Navegador</h2>
-            <button id="btn-consola">consola <></button>
+            <button id="btn-consola">consola</button>
          </div>
          <div class="ventana_consola hidden">Consola</div>
       </div>
@@ -55,119 +53,146 @@ document.querySelector('#app').innerHTML = `
   </main>
 `
 
+//Me traido todo de los dos paneles izq y derecho: body_bloque(cajaIzqDeR2),body_codigo(cajaDerechaDeR2),bloque_codigo(cajaIzqR1),navegador(cajaDerechaR1)
+const cajas = (function () {
+   let cajas = {}
+   cajas['contenedor'] = document.getElementById('contenedor');
+   cajas['panel_izq_r1'] = document.getElementById('bloque_codigo');
+   cajas['b_bloque_r2'] = document.getElementById('body_bloque');
+   cajas['b_cod_r2'] = document.getElementById('body_codigo');
+   cajas['panel_derecho_r1'] = document.getElementById('navegador');
+   return cajas;
+}());
 
-
-//Resizable
-const resizer_bloques_codigo = document.querySelector(".resizer_bloques_codigo"),
-   bloque_codigo = document.querySelector(".bloque_codigo"),
-   resizer_bloques = document.querySelector(".resizer_bloques"),
-   body_bloque = document.querySelector(".body_bloque"),
-   ventana_navegador = document.querySelector(".ventana_navegador")
-
-function addResizer(div_resizable, elemento_a_resaizear) {
-   // track current mouse position in x let
-   let x, w;
-   //manejador de evento para el clic del mouse
-   function rs_mousedownHandler(e) {
-      //Capturo posición horizontal del cursor en el eje X
-      x = e.clientX;
-      //getComputedStyle: obtengo los estilos computados del elemento con la clase elemento_a_resaizear y, específicamente, obtengo el ancho de este elemento.
-      let sbWidth = window.getComputedStyle(elemento_a_resaizear).width;
-      w = parseInt(sbWidth, 10); //convierto el valor obtenido a decimal
-
-      document.addEventListener("mousemove", rs_mousemoveHandler);//agrego el escucha al evento mousemove, para q se ejecute la fn rs_mousemoveHandler
-      document.addEventListener("mouseup", rs_mouseupHandler);//para limpiar y finaliza el seguimiento del movimiento
-   }
-
-   function rs_mousemoveHandler(e) {
-      let dx = e.clientX - x; //delta de la posicion de x
-      let cw = w + dx; // complete width
-      elemento_a_resaizear.style.width = `${cw}px`;
-      if (cw < 400) {
-         console.log("menor a 700")
-         //console.log(div_resizable.getAttribute("class"))
-      } else {
-         console.log("mayor")
+//Resizers
+let r1 = document.querySelector("#r1")
+let r2 = document.querySelector("#r2")
+let arrayDeResizers = [r1, r2]
+for (let r of arrayDeResizers) {
+   r.addEventListener('mousedown', e => {
+      e.preventDefault();
+      let resizer = e.target
+      document.addEventListener('mousemove', resize);
+      document.addEventListener('mouseup', e => {
+         document.removeEventListener('mousemove', resize);
+      });
+      function resize(e) {
+         //let body_width = 100 / document.body.clientWidth;
+         let panel_izq_r1_width = 100 / parseFloat(getComputedStyle(cajas.panel_izq_r1, '').width)
+         let contenedor_width = 100 / parseFloat(getComputedStyle(cajas.contenedor, '').width)
+         if (resizer.id == "r1") {
+            //r1 mueve los div bloque_codigo y navegador - r1 divide panel izq de panel derecho
+            let left_width = (parseFloat(getComputedStyle(cajas.panel_izq_r1, '').width) + e.movementX) * contenedor_width;
+            cajas.panel_izq_r1.style.width = left_width + '%';
+            cajas.panel_derecho_r1.style.width = (100 - left_width) + '%';
+            // cajas.panel_izq_r1.style.flex = "1 1 auto"
+            // cajas.panel_derecho_r1.style.flex = "1 1 auto"
+         } else {
+            //r2 mueve los divs body_bloque y body_codigo
+            let left_width = (parseFloat(getComputedStyle(cajas.b_bloque_r2, '').width) + e.movementX) * panel_izq_r1_width;
+            cajas.b_bloque_r2.style.width = left_width + '%';
+            cajas.b_cod_r2.style.width = (100 - left_width) + '%';
+            // cajas.b_cod_r2.style.width = `calc(100% - ${left_width}%)`
+            // cajas.b_cod_r2.style.flex = "1 1 auto"
+         }
       }
-   }
-
-   function rs_mouseupHandler() {
-      // remuevo event mousemove && mouseup
-      document.removeEventListener("mouseup", rs_mouseupHandler);
-      document.removeEventListener("mousemove", rs_mousemoveHandler);
-   }
-   div_resizable.addEventListener("mousedown", rs_mousedownHandler);
+   })
 }
-//Asocio los divs que van a resaizear la ventana
-addResizer(resizer_bloques_codigo, bloque_codigo);
-addResizer(resizer_bloques, body_bloque);
-
 
 //Habilito y deshabilito: body_codigo, body_bloque, ventana_navegador y ventana_consola -----------------
-const btnBloque = document.querySelector("#btn-bloque");
-const btnCodigo = document.querySelector("#btn-codigo");
-const btnNavegador = document.querySelector("#btn-navegador");
-const navegador = document.querySelector(".navegador")
-const btnConsola = document.querySelector("#btn-consola");
-const btn_html = document.querySelector("#btn-html");
-const btn_css = document.querySelector("#btn-css");
-const btn_js = document.querySelector("#btn-js");
-const div_bloque_codigo = document.querySelector(".bloque_codigo")
-
-function handelClickVisibility(e) {
-   //el e es el btn
-   //el btn-html, habilita y deshabilita 3 divs
-   const dicionario_ventana = { "btn-bloque": '.body_bloque', "btn-codigo": '.body_codigo', "btn-navegador": '.navegador', "btn-consola": ".ventana_consola"}
-   const elemento_ventana = document.querySelector(dicionario_ventana[e.target.id]);
-   elemento_ventana.classList.contains("hidden") ? elemento_ventana.classList.remove("hidden") : elemento_ventana.classList.add("hidden")
-   //habilita o deshabilita el btn cliqueado
-   e.target.classList.toggle("button_focus")
-
-   if (!btnBloque.classList.contains("button_focus") && !btnCodigo.classList.contains("button_focus")) {
-      //estan los dos apagados - oculto el div entero de bloque-codigo
-      bloque_codigo.classList.add("hidden")
-      div_bloque_codigo.classList.add("fondo_gris")
-   } else {
-      bloque_codigo.classList.remove("hidden")
-      div_bloque_codigo.classList.remove("fondo_gris")
-   }
-
-   //Si el btn Navegador esta apagado, ocultamos el resizer_bloques_codigo
-   !btnNavegador.classList.contains("button_focus") ? resizer_bloques_codigo.classList.add("hidden") : resizer_bloques_codigo.classList.remove("hidden")
-   //Si el btn Codigo esta apagado, ocultamos el resizer_bloques
-   !btnCodigo.classList.contains("button_focus") ? resizer_bloques.classList.add("hidden") : resizer_bloques.classList.remove("hidden")
+const btnsMayores = {
+   "btn-bloque": document.querySelector("#btn-bloque"),
+   "btn-codigo": document.querySelector("#btn-codigo"),
+   "btn-navegador": document.querySelector("#btn-navegador"),
+}
+const btnsMenores = {
+   "btn-html": document.querySelector("#btn-html"),
+   "btn-css": document.querySelector("#btn-css"),
+   "btn-js": document.querySelector("#btn-js"),
+   "btn-consola": document.querySelector("#btn-consola")
 }
 
-//fn para dentro del div bloques-codigo
-function handelClickVisibilityWorkSpaces(e){
-    //Adentro de bloques-codigo
-    const lista_elementos_btns = [btn_html, btn_css, btn_js]
-     //deshabilito todos los btns de bloques-codigo para luego habilitar solo el cliqueado
-     lista_elementos_btns.forEach(btn => {
+const div_bloque_codigo = document.querySelector(".bloque_codigo")
+const div_body_bloque = document.querySelector(".body_bloque")
+const div_body_codigo = document.querySelector(".body_codigo")
+const div_navegador = document.querySelector(".navegador")
+
+function prenderBtnSegunDiv(btn) {
+   const lista_id = btn.id == "btn-codigo" ? ["#codigo-html", "#codigo-css", "#codigo-js"] : ["#wsp-html", "#wsp-css", "#wsp-js"]
+   lista_id.forEach(elemento => {
+      const elemento_ventana = document.querySelector(elemento);
+      //!elemento_ventana.classList.contains("hidden") && console.log("este div esta prendido " + elemento_ventana.id)
+      if (!elemento_ventana.classList.contains("hidden")) {
+         btnsMenores["btn-" + elemento_ventana.id.split("-")[1]].classList.add("button_focus")
+      }
+   })
+}
+function handelClickVisibilityPanels(e) {
+   //el e es el btn
+   const dicionario_ventana = { "btn-bloque": '.body_bloque', "btn-codigo": '.body_codigo', "btn-navegador": '.navegador', "btn-consola": ".ventana_consola" }
+   const elemento_ventana = document.querySelector(dicionario_ventana[e.target.id]);
+   elemento_ventana.classList.contains("hidden") ? elemento_ventana.classList.remove("hidden") : elemento_ventana.classList.add("hidden")
+   e.target.classList.toggle("button_focus")
+   if (!btnsMayores["btn-bloque"].classList.contains("button_focus") && !btnsMayores["btn-codigo"].classList.contains("button_focus")) {
+      //estan los dos apagados - oculto el div entero de bloque-codigo
+      div_bloque_codigo.classList.add("hidden")
+      div_bloque_codigo.classList.add("fondo_gris")
+      //deshabilito los btns para que no queden prendidos
+      btnsMenores["btn-html"].classList.remove("button_focus")
+      btnsMenores["btn-css"].classList.remove("button_focus")
+      btnsMenores["btn-js"].classList.remove("button_focus")
+   } else {
+      div_body_codigo.style.width = !btnsMayores["btn-bloque"].classList.contains("button_focus") && btnsMayores["btn-codigo"].classList.contains("button_focus") && "100%"
+      div_bloque_codigo.classList.remove("hidden")
+      div_bloque_codigo.classList.remove("fondo_gris")
+      btnsMayores["btn-bloque"].classList.contains("button_focus") && prenderBtnSegunDiv(e.target)
+      btnsMayores["btn-codigo"].classList.contains("button_focus") && prenderBtnSegunDiv(e.target)
+   }
+
+   //Si el btn Navegador esta apagado, ocultamos el resizer1
+   if (!btnsMayores["btn-navegador"].classList.contains("button_focus")) {
+      r1.classList.add("hidden")
+      div_bloque_codigo.style.width = "100%"
+   } else {
+      r1.classList.remove("hidden")
+      div_bloque_codigo.style.width = "50%"
+      div_navegador.style.width = "50%"
+   }
+   //Si el btn Codigo esta apagado, ocultamos el resizer2
+   !btnsMayores["btn-codigo"].classList.contains("button_focus") ? r2.classList.add("hidden") : r2.classList.remove("hidden")
+
+}
+
+//fn para dentro del div bloques-codigo (div izquierdo grande)
+function handelClickVisibilityWorkSpaces(e) {
+   //Adentro de bloques-codigo
+   const lista_elementos_btns = [btnsMenores["btn-html"], btnsMenores["btn-css"], btnsMenores["btn-js"]]
+   //deshabilito todos los btns de bloques-codigo para luego habilitar solo el cliqueado
+   lista_elementos_btns.forEach(btn => {
       btn.classList.remove("button_focus")
    })
    e.target.classList.add("button_focus")
 
    //oculto todos los wsp y todos los div de codigo
-   const lista_id_wp = ["#wsp-html", "#wsp-css", "#wsp-js","#codigo-html","#codigo-css","#codigo-js" ]
+   const lista_id_wp = ["#wsp-html", "#wsp-css", "#wsp-js", "#codigo-html", "#codigo-css", "#codigo-js"]
    lista_id_wp.forEach(elemento => {
       const elemento_ventana = document.querySelector(elemento);
       elemento_ventana.classList.add("hidden")
    })
 
    //habilito el wsp y div-codigo que corresponde al btn cliqueado
-   const diccionario_wsp_bloques ={ "btn-html": '#wsp-html',"btn-css": '#wsp-css',"btn-js": '#wsp-js'},
-   diccionario_wsp_codigo={"btn-html": '#codigo-html',"btn-css": '#codigo-css',"btn-js": '#codigo-js'}
+   const diccionario_wsp_bloques = { "btn-html": '#wsp-html', "btn-css": '#wsp-css', "btn-js": '#wsp-js' },
+      diccionario_wsp_codigo = { "btn-html": '#codigo-html', "btn-css": '#codigo-css', "btn-js": '#codigo-js' }
    const elemento_ventana_wsp = document.querySelector(diccionario_wsp_bloques[e.target.id]);
    elemento_ventana_wsp.classList.remove("hidden")
    const elemento_ventana_codigo = document.querySelector(diccionario_wsp_codigo[e.target.id]);
    elemento_ventana_codigo.classList.remove("hidden")
 }
 
-btnBloque.addEventListener("click", handelClickVisibility);
-btnCodigo.addEventListener("click", handelClickVisibility);
-btnNavegador.addEventListener("click", handelClickVisibility);
-btnConsola.addEventListener("click", handelClickVisibility);
-btn_html.addEventListener("click", handelClickVisibilityWorkSpaces)
-btn_css.addEventListener("click", handelClickVisibilityWorkSpaces)
-btn_js.addEventListener("click", handelClickVisibilityWorkSpaces)
+btnsMayores["btn-bloque"].addEventListener("click", handelClickVisibilityPanels);
+btnsMayores["btn-codigo"].addEventListener("click", handelClickVisibilityPanels);
+btnsMayores["btn-navegador"].addEventListener("click", handelClickVisibilityPanels);
+btnsMenores["btn-consola"].addEventListener("click", handelClickVisibilityPanels);
+btnsMenores["btn-html"].addEventListener("click", handelClickVisibilityWorkSpaces)
+btnsMenores["btn-css"].addEventListener("click", handelClickVisibilityWorkSpaces)
+btnsMenores["btn-js"].addEventListener("click", handelClickVisibilityWorkSpaces)
