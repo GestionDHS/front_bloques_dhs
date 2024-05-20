2a6a41999b76340478252b625dd89dd5d261ed72
María Pía Achigar <achigarpia@gmail.com>
2024-05-20 15:08:26 -0300

    gp-pages

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,28 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+// import html from '@rollup/plugin-html';
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
-// const procedure_path= resolve(__dirname, '@blockly/block-shareable-procedures/');
-// console.log(procedure_path)
-// import {registerProcedureSerializer} from ${procedure_path};
-/*saco la base
-root:"src"
-outDir:"../dist"
-*/
+
 export default defineConfig({
-  // plugins: [
-  //   registerProcedureSerializer(),
-  // ],
-  base: "/front_bloques_dhs/",
   root,
   build: {
     outDir,
     emptyOutDir: true,
     rollupOptions: {
       input: {
+        // main:"src/index.html",
         main: resolve(root, "./index.html"),
-        //main: resolve(__dirname, './index.html'),
         primerPaso:(root, "pages", "frontPrimerPaso", "index.html"),
         act1: resolve(root, "pages", "actividad001", "act.html"),
       }
     },
+    // plugins: [
+    //   html({
+    //     title: 'front_bloques_dhs',
+    //     template: 'src/index.html' 
+    //   })
+    // ]
   },
     assetsDir: "img",
     assetsInclude: [
@@ -33,4 +30,5 @@ export default defineConfig({
       "./interpreter/interpreter.js",
       "**/*.css?type=text/css",
     ],
+    base: "/front_bloques_dhs/",
 });
